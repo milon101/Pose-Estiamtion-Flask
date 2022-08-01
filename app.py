a2e5ada0bac8102683264feb7ca5cd71f13b59da
milon101 <milon.imdad@gmail.com>
2022-08-01 23:18:36 +0100

    modified

--- app.py
+++ app.py
@@ -25,33 +25,33 @@ def hello():
     return 'Hello World!'
 
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     if request.method == 'POST':
-#         r = request
-#         # convert string of image data to uint8
-#         nparr = np.fromstring(r.data, np.uint8)
-#         # decode image
-#         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        r = request
+        # convert string of image data to uint8
+        nparr = np.fromstring(r.data, np.uint8)
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-#         # file = request.files['file'].stream
-#         # img_bytes = file.read()
-#         landmarks = get_prediction(img)
+        # file = request.files['file'].stream
+        # img_bytes = file.read()
+        landmarks = get_prediction(img)
 
-#         print(landmarks, file=sys.stderr)
-#         print(landmarks[0], file=sys.stderr)
-#         # d = {0:[landmarks[0].x, landmarks[0].y]}
-#         d = {}
-#         for i,l in enumerate(landmarks):
-#             d[i] = [l.x, l.y]
-#         print(d, file=sys.stderr)
-#         json_landmarks = json.dumps(d)
-#         print('json_landmarks:', json_landmarks , file=sys.stderr)
-#         # response = {'landmarks': landmarks}
-#         # encode response using jsonpickle
-#         # response_pickled = jsonpickle.encode(response)
-#         # return Response(response=response_pickled, status=200, mimetype="application/json")
-#         return json_landmarks
+        print(landmarks, file=sys.stderr)
+        print(landmarks[0], file=sys.stderr)
+        # d = {0:[landmarks[0].x, landmarks[0].y]}
+        d = {}
+        for i,l in enumerate(landmarks):
+            d[i] = [l.x, l.y]
+        print(d, file=sys.stderr)
+        json_landmarks = json.dumps(d)
+        print('json_landmarks:', json_landmarks , file=sys.stderr)
+        # response = {'landmarks': landmarks}
+        # encode response using jsonpickle
+        # response_pickled = jsonpickle.encode(response)
+        # return Response(response=response_pickled, status=200, mimetype="application/json")
+        return json_landmarks
 
 if __name__ == '__main__':
     app.run(debug=True)
